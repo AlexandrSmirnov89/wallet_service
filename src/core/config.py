13ed37e183204148a987faf8c5a75cbcd5458567
AlexandrSmirnov89 <smirnov1sasha@gmail.com>
2025-02-25 19:44:38 +0500

    update config

--- src/core/config.py
+++ src/core/config.py
@@ -1,9 +1,14 @@
 from pydantic import ConfigDict
 from pydantic_settings import BaseSettings
+from environs import Env
+
+
+env = Env()
+env.read_env()
 
 class Settings(BaseSettings):
-    DATABASE_URL: str = "postgresql+asyncpg://wallet_admin:qwerty@localhost:5432/wallet_service"
-    REDIS_URL: str = "redis://localhost:6379/0"
+    DATABASE_URL: str = env['DATABASE_URL']
+    REDIS_URL: str = env['REDIS_URL']
     model_config = ConfigDict(env_file=".env")
 
 
