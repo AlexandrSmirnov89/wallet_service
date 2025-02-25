a2539eb826705fb61ff7f41e940559bb9919698d
AlexandrSmirnov89 <smirnov1sasha@gmail.com>
2025-02-25 11:20:17 +0500

    deploy version

--- src/core/config.py
+++ src/core/config.py
@@ -2,8 +2,8 @@ from pydantic import ConfigDict
 from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
-    DATABASE_URL: str = "postgresql+asyncpg://wallet_admin:qwerty@db:5432/wallet_service"
-    REDIS_URL: str = "redis://redis:6379/0"
+    DATABASE_URL: str = "postgresql+asyncpg://wallet_admin:qwerty@localhost:5432/wallet_service"
+    REDIS_URL: str = "redis://localhost:6379/0"
     model_config = ConfigDict(env_file=".env")
 
 
