4094fbc904bb9cefb23f3b3058a2929f98be269b
AlexandrSmirnov89 <smirnov1sasha@gmail.com>
2025-02-25 11:14:35 +0500

    deploy version

--- src/core/celery_config.py
+++ src/core/celery_config.py
@@ -2,15 +2,15 @@ from celery import Celery
 
 celery_app = Celery(
     'wallet_service_v2',
-    broker='redis://redis:6379/0',
-    backend='redis://redis:6379/0',
+    broker='redis://localhost:6379/0',
+    backend='redis://localhost:6379/0',
     include=["src.api.v1.tasks.wallet"]
 )
 
 celery_app.conf.update(
     task_serializer='json',
     accept_content=['json'],
-    result_backend='redis://redis:6379/0',
+    result_backend='redis://localhost:6379/0',
     timezone='UTC',
     broker_connection_retry_on_startup=True
 )
